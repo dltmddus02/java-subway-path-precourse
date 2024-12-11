6713e4d0045f6ab6e65dba7b83336dff412200bb
dltmddus <tmddus4671@gmail.com>
2024-12-12 08:21:02 +0900

    feat(SubwayController) : 주어진 지하철 정보 저장
    - LineRepository에 호선 정보 저장
    - StationRepository에 역 정보 저장

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -1,7 +1,15 @@
 package subway.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Supplier;
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.service.CalculateMinimumCostPath;
 import subway.view.input.InputValidator;
 import subway.view.input.exception.InputException;
@@ -17,7 +25,7 @@ public class SubwayController {
     }
 
     public void run(Scanner scanner) {
-//        setupSubwayStation();
+        setupSubwayStation();
 
         OutputView.printMainScreen();
         String mainFeature = chooseMainFeature(scanner);
@@ -82,6 +90,66 @@ public class SubwayController {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private void setupSubwayStation() {
+        Station 교대역 = new Station("교대역");
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+        Station 남부터미널역 = new Station("남부터미널역");
+        Station 양재역 = new Station("양재역");
+        Station 매봉역 = new Station("매봉역");
+        Station 양재시민의숲역 = new Station("양재시민의숲역");
+
+        StationRepository.addStation(교대역);
+        StationRepository.addStation(강남역);
+        StationRepository.addStation(역삼역);
+        StationRepository.addStation(남부터미널역);
+        StationRepository.addStation(양재역);
+        StationRepository.addStation(매봉역);
+        StationRepository.addStation(양재시민의숲역);
+
+        Map<Station, Map<Station, List<Integer>>> adjacent교대역 = new HashMap<>();
+        List<Map<Integer, Integer>> 교대역인접정보 = new ArrayList<>();
+        adjacent교대역.put(강남역, Map.of(교대역, List.of(2, 3)));
+        adjacent교대역.put(남부터미널역, Map.of(교대역, List.of(3, 2)));
+        교대역.setAdjacentStations(adjacent교대역);
+
+        Map<Station, Map<Station, List<Integer>>> adjacent강남역 = new HashMap<>();
+        adjacent강남역.put(역삼역, Map.of(강남역, List.of(2, 3)));
+        adjacent강남역.put(양재역, Map.of(강남역, List.of(2, 8)));
+        강남역.setAdjacentStations(adjacent강남역);
+
+        Map<Station, Map<Station, List<Integer>>> adjacent역삼역 = new HashMap<>();
+        List<Map<Integer, Integer>> 역삼역인접정보 = new ArrayList<>();
+        역삼역.setAdjacentStations(adjacent역삼역);
+
+        Map<Station, Map<Station, List<Integer>>> adjacent남부터미널역 = new HashMap<>();
+        List<Map<Integer, Integer>> 남부터미널역인접정보 = new ArrayList<>();
+        adjacent남부터미널역.put(양재역, Map.of(남부터미널역, List.of(6, 5)));
+        남부터미널역.setAdjacentStations(adjacent남부터미널역);
+
+        Map<Station, Map<Station, List<Integer>>> adjacent양재역 = new HashMap<>();
+        List<Map<Integer, Integer>> 양재역인접정보 = new ArrayList<>();
+        양재역인접정보.add(new HashMap<>(Map.of(1, 1)));  // 남부터미널역 (6호선, 1호선)
+        양재역인접정보.add(new HashMap<>(Map.of(10, 3)));  // 남부터미널역 (6호선, 1호선)
+        adjacent양재역.put(매봉역, Map.of(매봉역, List.of(1, 1)));
+        adjacent양재역.put(양재시민의숲역, Map.of(매봉역, List.of(10, 3)));
+        양재역.setAdjacentStations(adjacent양재역);
+
+        Map<Station, Map<Station, List<Integer>>> adjacent매봉역 = new HashMap<>();
+        매봉역.setAdjacentStations(adjacent매봉역);
+
+        Map<Station, Map<Station, List<Integer>>> adjacent양재시민의숲역 = new HashMap<>();
+        양재시민의숲역.setAdjacentStations(adjacent양재시민의숲역);
+
+        Line line1 = new Line("2호선");
+        Line line2 = new Line("3호선");
+        Line line3 = new Line("신분당선");
+
+        LineRepository.addLine(line1);
+        LineRepository.addLine(line2);
+        LineRepository.addLine(line3);
     }
 
 }
