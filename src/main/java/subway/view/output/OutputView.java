e870a89e5bc32c919b6e97a3e716172367e34055
dltmddus <tmddus4671@gmail.com>
2024-12-12 08:28:01 +0900

    feat(OutputView) : 조회 결과 출력 기능 구현

--- src/main/java/subway/view/output/OutputView.java
+++ src/main/java/subway/view/output/OutputView.java
@@ -1,10 +1,16 @@
 package subway.view.output;
 
+import static subway.view.output.OutputMessage.INFO_PREFIX;
+import static subway.view.output.OutputMessage.INFO_SEPARATOR;
+import static subway.view.output.OutputMessage.INFO_TOTAL_DISTANCE;
+import static subway.view.output.OutputMessage.INFO_TOTAL_TIME;
 import static subway.view.output.OutputMessage.INPUT_ARRIVAL_STATION;
 import static subway.view.output.OutputMessage.INPUT_DEPARTURE_STATION;
 import static subway.view.output.OutputMessage.INPUT_FEATURE;
 import static subway.view.output.OutputMessage.MAIN_SCREEN;
 import static subway.view.output.OutputMessage.PATH_CRITERIA;
+
+import java.util.List;
 
 public class OutputView {
     public static void printMainScreen() {
@@ -25,5 +31,16 @@ public class OutputView {
 
     public static void printArrivalStation() {
         System.out.println(INPUT_ARRIVAL_STATION.getMessage());
+    }
+
+    public static void printResult(Integer totalDistance, Integer totalTime, List<String> stations) {
+        System.out.println(INFO_PREFIX.getMessage() + INFO_SEPARATOR.getMessage());
+        System.out.printf(INFO_PREFIX.getMessage() + INFO_TOTAL_DISTANCE.getMessage(), totalDistance);
+        System.out.printf(INFO_PREFIX.getMessage() + INFO_TOTAL_TIME.getMessage(), totalTime);
+        System.out.println(INFO_PREFIX.getMessage() + INFO_SEPARATOR.getMessage());
+
+        for (String station : stations) {
+            System.out.println(INFO_PREFIX.getMessage() + station);
+        }
     }
 }
