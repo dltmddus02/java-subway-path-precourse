dd84ec0707974d491da06e42ca39a3235b120546
dltmddus <tmddus4671@gmail.com>
2024-12-12 08:30:26 +0900

    feat(Application) : Application 구현
    - main 메서드 추가
    - SubwayController 인스턴스 생성, run 호출로 애플리케이션 실행

---
+++ src/main/java/subway/Application.java
@@ -0,0 +1,13 @@
+package subway;
+
+import java.util.Scanner;
+import subway.controller.SubwayController;
+
+public class Application {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        SubwayController subwayController = new SubwayController();
+        subwayController.run(scanner);
+    }
+}
