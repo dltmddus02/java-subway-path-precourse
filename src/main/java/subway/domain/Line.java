9774cfa53634e731efde89f46ed5346bc75d2690
dltmddus <tmddus4671@gmail.com>
2024-12-12 07:55:54 +0900

    feat(Product) : 지하철 호선 정보 저장하는 Line 도메인 생성

---
+++ src/main/java/subway/domain/Line.java
@@ -0,0 +1,13 @@
+package subway.domain;
+
+public class Line {
+    private String name;
+
+    public Line(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
