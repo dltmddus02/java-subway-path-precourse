a5979c475ab73cb915d05685b6538998900f58e7
dltmddus <tmddus4671@gmail.com>
2024-12-12 08:06:13 +0900

    feat(InputValidator) : 원하는 기능 입력 검증 구현

---
+++ src/main/java/subway/view/input/InputValidator.java
@@ -0,0 +1,20 @@
+package subway.view.input;
+
+import subway.domain.Station;
+import subway.domain.StationRepository;
+import subway.view.input.exception.InputErrorMessage;
+import subway.view.input.exception.InputException;
+
+public class InputValidator {
+    public static void validateNotNullOrEmpty(String input) {
+        if (input == null || input.isBlank()) {
+            throw new InputException(InputErrorMessage.INVALID_INPUT);
+        }
+    }
+
+    public static void validateMainFeature(String input) {
+        if (!input.equals("1") && !input.equals("Q")) {
+            throw new InputException(InputErrorMessage.INVALID_INPUT);
+        }
+    }
+}
